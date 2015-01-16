a8ca532cb79948044c61d1c69b08f9a7d8b5d965
ka-s <using0namespace0kas@gmail.com>
2015-01-17 00:15:34 +0900

    Make the dice random

--- Your_destiny_is_determined_by_the_random/Test.cpp
+++ Your_destiny_is_determined_by_the_random/Test.cpp
@@ -1,12 +1,16 @@
 // ================================================================
 //  Game make test
 // ================================================================
-# include <Siv3D.hpp>
+#include <Siv3D.hpp>
+#include <random>
+
+using namespace std;
 
 void Main(){
 
+    // ---------------- Window status ----------------
     // Background color
-    Graphics::SetBackground(Palette::White);
+    Graphics::SetBackground(Palette::Black);
     // Window title
     Window::SetTitle(L"Game make test");
     // Window size
@@ -14,7 +18,21 @@ void Main(){
     // Window set center
     Window::Centering();
 
+    // ---------------- Make the random ----------------
+    // Make the random
+    random_device rd;
+    // Mersenne twister
+    mt19937 mt(rd());
+    // Make a dice
+    uniform_int_distribution<int> dice(0, 6);
+
+    // Make the dice font
+    Font f_dice(16);
+
     while (System::Update()){
+
+        // Draw the dice
+        f_dice(dice(mt)).draw();
 
     }
 }
