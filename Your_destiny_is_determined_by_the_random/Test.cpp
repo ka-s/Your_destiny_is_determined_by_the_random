51b4cc2a0677cd65a66c7972a6c7e7f41206120d
ka-s <using0namespace0kas@gmail.com>
2015-01-17 14:01:44 +0900

    画面管理クラスの作成

--- Your_destiny_is_determined_by_the_random/Test.cpp
+++ Your_destiny_is_determined_by_the_random/Test.cpp
@@ -8,11 +8,74 @@
 using namespace std;
 using namespace boost;
 
+// ---------------- グローバル変数 ----------------
+namespace{
+    // サイコロを投げるフラグ
+    dynamic_bitset<> flag_dice(1);
+    // サイコロの結果
+    int dice_result;
+    // 現在のマス
+    int now_position = 0;
+
+    // ---------------- ランダム生成 ----------------
+    // ランダムデバイスを取得
+    random_device rd;
+    // メルセンヌツイスター
+    random::mt19937 mt(rd());
+    // サイコロを定義
+    random::uniform_int_distribution<> dice(1, 6);
+}
+
+// ================================
+//  画面管理クラス
+// ================================
+class ScreneManager{
+private:
+    // サイコロのフォント
+    Font f_dice;
+
+public:
+    // コンストラクタ
+    ScreneManager(){
+        // サイコロ用フォント作成
+        f_dice = Font(16);
+    }
+    // デストラクタ
+    ~ScreneManager(){}
+
+    // 更新メソッド
+    void update(){
+        // Zキーが押されたならば
+        if (Input::KeyZ.clicked){
+            // サイコロを投げるフラグをオン
+            flag_dice.set(0);
+            // サイコロの値を取得
+            dice_result = dice(mt);
+            // コマを進める
+            now_position += dice_result;
+        }
+    }
+
+    // 描画メソッド
+    void draw(){
+        // サイコロを投げるフラグがオンなら
+        if (flag_dice.test(0)){
+            // サイコロを描画
+            f_dice(dice_result).draw();
+            // 現在のマスを描画
+            f_dice(now_position).draw({ 0, 18 });
+        }
+        else{
+            // 最初のメッセージを描画
+            f_dice(L"Please press Z key").draw();
+        }
+    }
+};
+
 // --------------------------------
 //  メイン関数
 // --------------------------------
 void Main(){
-
     // ---------------- ウィンドウ情報 ----------------
     // 背景色
     Graphics::SetBackground(Palette::Black);
@@ -23,48 +86,16 @@ void Main(){
     // ウィンドウを中心にセット
     Window::Centering();
 
-    // ---------------- ランダム生成 ----------------
-    // ランダムデバイスを取得
-    random_device rd;
-    // メルセンヌツイスター
-    random::mt19937 mt(rd());
-    // サイコロを定義
-    random::uniform_int_distribution<> dice(1, 6);
-
     // ---------------- 変数 ----------------
-    // サイコロのフォント
-    Font f_dice(16);
-    // サイコロを投げるフラグ
-    dynamic_bitset<> flag_dice(1);
-    // サイコロの結果
-    int dice_result;
-    // 現在のマス
-    int now_position = 0;
+    // 画面管理ポインタ
+    std::shared_ptr<ScreneManager> screne_manager = make_shared<ScreneManager>();
 
     // ---------------- メインループ ----------------
     while (System::Update()){
+        // 更新
+        screne_manager->update();
 
-        // Zキーが押されたならば
-        if (Input::KeyZ.clicked){
-            // サイコロを投げるフラグをオン
-            flag_dice.set(0);
-            // サイコロの値を取得
-            dice_result = dice(mt);
-            // コマを進める
-            now_position += dice_result;
-        }
-
-        // サイコロを投げるフラグがオンなら
-        if (flag_dice.test(0)){
-            // サイコロを描画
-            f_dice(dice_result).draw();
-            // 現在のマスを描画
-            f_dice(now_position).draw({0, 18});
-        }
-        else{
-            // 最初のメッセージを描画
-            f_dice(L"Please press Z key").draw();
-        }
-
+        // 描画
+        screne_manager->draw();
     }
 }
